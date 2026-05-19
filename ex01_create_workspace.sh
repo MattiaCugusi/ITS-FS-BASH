4b4ffcc1fd54b74798d1fcb1e740920266b3ce87
Mattia Cugusi <m.cugusi287@gmail.com>
2026-05-19 16:30:55 +0200

    es.1

---
+++ ex01_create_workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir workspace
+mkdir workspace/documents/
+mkdir workspace/downloads/
+mkdir workspace/images/
+mkdir workspace/scripts/
+mkdir workspace/temp/
+
+touch workspace/documents/notes.txt
+touch workspace/downloads/list.txt
+touch workspace/images/image-info.txt
+touch workspace/scripts/todo.sh
+touch workspace/temp/tmp.txt
+
+echo Workspace created successfully
