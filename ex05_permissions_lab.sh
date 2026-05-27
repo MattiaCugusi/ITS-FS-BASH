c6984a0db2290acb82b7605d92fdf5b03eda206c
Mattia Cugusi <m.cugusi287@gmail.com>
2026-05-27 16:35:01 +0200

    es. 5

---
+++ ex05_permissions_lab.sh
@@ -0,0 +1,12 @@
+
+mkdir permissions-lab
+
+touch permissions-lab/public.txt
+touch permissions-lab/private.txt
+touch permissions-lab/script.sh
+
+chmod 666 permission-lab/pubic.txt
+chmod 600 permissions-lab/private.txt
+chmod u+x permissions-lab/script.sh
+
+ls -la permissions-lab
